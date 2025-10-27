d48347404f01d3c1f89ac2298827cadaeeffe6d4
Guresh12 <gureshnathil@gmail.com>
2025-10-27 15:29:03 +0300

    Update index.tsx

--- Hozn-RealEstate-Fullstack/src/components/inner-pages/about-us/about-us-one/index.tsx
+++ Hozn-RealEstate-Fullstack/src/components/inner-pages/about-us/about-us-one/index.tsx
@@ -3,27 +3,27 @@ import HeaderOne from "@/layouts/headers/HeaderOne"
 import BLockFeatureOne from "./BLockFeatureOne"
 import VideoBanner from "@/components/homes/home-seven/VideoBanner"
 import BLockFeatureFive from "@/components/homes/home-one/BLockFeatureFive"
-import Feedback from "@/components/homes/home-five/"
+import Feedback from "@/components/homes/home-five"
 import AgentArea from "@/components/homes/home-one/AgentArea"
 import Brand from "./Brand"
 import FooterFour from "@/layouts/footers/FooterFour"
 import FancyBanner from "@/components/common/FancyBanner"
 
 const AboutUsOne = () => {
-   return (
-      <>
-         <HeaderOne style={true} />
-         <BreadcrumbOne title="About Agency" sub_title="About us" style={false} />
-         <BLockFeatureOne />
-         <VideoBanner />
-         <BLockFeatureFive style={true} />
-         <Feedback style={true} />
-         <AgentArea style={false} />
-         <Brand />
-         <FancyBanner style={false} />
-         <FooterFour />
-      </>
-   )
+  return (
+    <>
+      <HeaderOne />
+      <BreadcrumbOne title="About Agency" sub_title="About us" />
+      <BLockFeatureOne />
+      <VideoBanner />
+      <BLockFeatureFive />
+      <Feedback />
+      <AgentArea />
+      <Brand />
+      <FancyBanner />
+      <FooterFour />
+    </>
+  )
 }
 
 export default AboutUsOne
